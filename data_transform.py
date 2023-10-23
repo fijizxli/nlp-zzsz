3f13d889ebff3a7f2301937bb3d069d4244e1f18
justoneguy01 <90860513+justoneguy01@users.noreply.github.com>
2023-10-23 12:36:19 +0200

    Update data_transform.py Get the movie information and added to the main dataframe.
    Get the movie information and added to the main dataframe.

--- data_transform.py
+++ data_transform.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 import pandas as pd
 import requests
@@ -10,7 +11,8 @@ def get_rotten_tomatoes_html(movie_url):
         response = requests.get(movie_url)
 
         if response.status_code == 200:
-            html_raw = remove_blank_lines(response.text) 
+            #html_raw = remove_blank_lines(response.text) 
+            html_raw = response.text
             return html_raw
         else:
             print("Failed to fetch data from Rotten Tomatoes. Status code:", response.status_code)
@@ -21,11 +23,11 @@ def get_rotten_tomatoes_html(movie_url):
         return None
     
 
-def remove_blank_lines(text):
+""" def remove_blank_lines(text):
     if text:
         cleaned_text = re.sub(r'\n\s*\n', '\n', text)  # Remove blank lines
         return cleaned_text
-    return None
+    return None """
 
 
 def get_comments(html_code, movie_title):
@@ -52,23 +54,52 @@ def get_comments(html_code, movie_title):
     return pd.DataFrame(data)
 
 
-def main():
+def get_movie_info(html_code):
+    movie_info = {}
+
+    soup = BeautifulSoup(html_code, 'html.parser')
+
+    info_list = soup.find('ul', id='info')
+
+    for info_item in info_list.find_all('li', {'data-qa': 'movie-info-item'}):
+        label = info_item.find('b', {'data-qa': 'movie-info-item-label'}).text.strip()
+        value = info_item.find('span', {'data-qa': 'movie-info-item-value'}).text.strip()
+        movie_info[label] = value
+
+    return pd.DataFrame([movie_info])
+
+def soure_reader():
     movie_titles = list()
     with open("source.txt", 'r') as file:
         for line in file:
             movie_titles.append(line.strip()) 
-    
+    return movie_titles
+
+
+def main():
+    movie_titles = soure_reader()
     reviews_df = pd.DataFrame()
 
     for movie in movie_titles:
-        movie_url = f"https://www.rottentomatoes.com/m/{movie}/reviews"
-        html_code = get_rotten_tomatoes_html(movie_url)
-        df = get_comments(html_code, movie)
+        i = 0
+        url_for_comments = f"https://www.rottentomatoes.com/m/{movie}/reviews"
+        url_for_the_movie_info = f"https://www.rottentomatoes.com/m/{movie}"
 
-        reviews_df = pd.concat([reviews_df, df], ignore_index=True)
+        html_code_comments = get_rotten_tomatoes_html(url_for_comments)
+        html_code_movie_info = get_rotten_tomatoes_html(url_for_the_movie_info)
+
+        comments_df = get_comments(html_code_comments, movie)
+        movie_info_df = get_movie_info(html_code_movie_info)
+
+        for index, row in comments_df.iterrows():
+            if row['Movie'] == movie:
+                i+=1
+
+        movie_info_df = pd.concat([movie_info_df] * i, ignore_index=True)
+        comments_df = comments_df.join(movie_info_df)
+        reviews_df = pd.concat([reviews_df, comments_df], ignore_index=True)
 
     print(reviews_df)
-    
     reviews_df.to_excel("reviews.xlsx", index=False)
 
 
